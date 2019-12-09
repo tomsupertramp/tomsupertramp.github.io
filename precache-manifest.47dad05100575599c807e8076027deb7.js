e5fed49bf53c7cf1a0b215d2b246959adc875522
Artem Mukhamatyanov <ausupertramp@gmail.com>
2019-12-09 22:16:40 +1000

    travelask

---
+++ precache-manifest.47dad05100575599c807e8076027deb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118360f03543b125196bbbc4a79ab65d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8ccd35ee7c08ab047c8",
+    "url": "/static/css/2.c5bf9ada.chunk.css"
+  },
+  {
+    "revision": "902e410f9c38b13d4d7c",
+    "url": "/static/css/main.5df7d988.chunk.css"
+  },
+  {
+    "revision": "b8ccd35ee7c08ab047c8",
+    "url": "/static/js/2.d939983d.chunk.js"
+  },
+  {
+    "revision": "29a1d98eb64b1e6d3217be6828297cfe",
+    "url": "/static/js/2.d939983d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "902e410f9c38b13d4d7c",
+    "url": "/static/js/main.962d17d4.chunk.js"
+  },
+  {
+    "revision": "6c23cdbe43a760e45c87",
+    "url": "/static/js/runtime-main.bfc25c97.js"
+  }
+]);
